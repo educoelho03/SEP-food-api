e8039196539f5eaf3a768095c0c9a63068fd95b4
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-06-19 13:46:15 -0300

    Tornando o @Autowired opcional

---
+++ 2.15. Dependência opcional com @Autowired/src/main/java/com/example/restaurante/domain/service/AtivacaoClienteService.java
@@ -0,0 +1,26 @@
+package com.example.restaurante.domain.service;
+
+import com.example.restaurante.domain.modelo.Cliente;
+import com.example.restaurante.domain.notification.Notificador;
+import com.example.restaurante.domain.notification.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    @Autowired(required = false) // torna essa dependencia opcional
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+
+        System.out.println("AtivacaoClienteService " + notificador);
+    }
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+}
