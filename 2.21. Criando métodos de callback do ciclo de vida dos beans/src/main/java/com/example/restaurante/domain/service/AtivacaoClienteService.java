3bac311907ae340e93d758ddb0dccb5ecea45fb0
educoelho03 <eduardopaivacoelho@outlook.com>
2024-06-20 10:32:42 -0300

    colocando ciclo de vida nos beans

--- 2.21. Criando métodos de callback do ciclo de vida dos beans/src/main/java/com/example/restaurante/domain/service/AtivacaoClienteService.java
+++ 2.21. Criando métodos de callback do ciclo de vida dos beans/src/main/java/com/example/restaurante/domain/service/AtivacaoClienteService.java
@@ -7,15 +7,29 @@ import com.example.restaurante.domain.notification.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
+    //	@PostConstruct
+    public void init() {
+        System.out.println("INIT " + notificador);
+    }
+
+    //	@PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+
     public void ativar(Cliente cliente) {
         cliente.ativar();
+
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
 }
