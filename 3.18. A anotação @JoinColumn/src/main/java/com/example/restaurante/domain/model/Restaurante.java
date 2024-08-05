cd6cdd2ff2bf7142f833cd18596637e3059c6daa
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-05 11:27:22 -0300

    adicionando a anotação JoinColumn

---
+++ 3.18. A anotação @JoinColumn/src/main/java/com/example/restaurante/domain/model/Restaurante.java
@@ -0,0 +1,27 @@
+package com.example.restaurante.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id")
+    private Cozinha cozinha;
+}
