9c24e2ebb373c320fa36590aa2f08a9bd7b67c14
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-16 11:04:05 -0300

    implementado content negotiation

---
+++ 4.13. Implementando content negotiation para retornar JSON ou XML/src/main/java/com/example/restaurante/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.example.restaurante.api.controller;
+
+import com.example.restaurante.domain.model.Cozinha;
+import com.example.restaurante.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+}
---
+++ 4.13. Implementando content negotiation para retornar JSON ou XML/src/main/java/com/example/restaurante/domain/repository/CidadeRepository.java
@@ -0,0 +1,15 @@
+package com.example.restaurante.domain.repository;
+
+import com.example.restaurante.domain.model.Cidade;
+import com.example.restaurante.domain.model.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CidadeRepository  {
+
+    List<Cidade> listar();
+    Cidade buscar(Long id);
+    Cidade salvar(Cidade cidade);
+    void remover(Cidade cidade);
+}
