0e6100ac6c910a4b43f0605f3c7090511c0ee1a1
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-16 10:40:11 -0300

    criado controller cozinha

---
+++ 3.19. Propriedade nullable de @Column e @JoinColumn/src/main/java/com/example/restaurante/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.example.restaurante.api.controller;
+
+import com.example.restaurante.domain.model.Cozinha;
+import com.example.restaurante.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+}
