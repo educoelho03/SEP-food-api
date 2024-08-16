556c84bcafd8f85c41b3cec12e173468bb3f7ee8
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-16 11:34:45 -0300

    implementando pathVariable no metodo buscar

---
+++ 4.14. Consultando Singleton Resource com GET e @PathVariable/src/main/java/com/example/restaurante/api/controller/CozinhaController.java
@@ -0,0 +1,30 @@
+package com.example.restaurante.api.controller;
+
+import com.example.restaurante.domain.model.Cozinha;
+import com.example.restaurante.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id){
+        return cozinhaRepository.buscar(id);
+    }
+}
