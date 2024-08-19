d4be23b9696b28f12c01526431a4cf02147aaca9
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-19 10:12:03 -0300

    Adicionando metodo DELETE, deletar uma cozinha

---
+++ 4.26. Modelando e implementando a exclusão de recursos com DELETE/src/main/java/com/example/restaurante/jpa/formaPagamento/ConsultaFormaPagamentoMain.java
@@ -0,0 +1,27 @@
+package com.example.restaurante.jpa.formaPagamento;
+
+import com.example.restaurante.RestauranteApplication;
+import com.example.restaurante.domain.model.FormaPagamento;
+import com.example.restaurante.domain.repository.FormaPagamentoRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaFormaPagamentoMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(RestauranteApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        FormaPagamentoRepository formaPagamentoRepository = applicationContext.getBean(FormaPagamentoRepository.class);
+
+        List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.listar();
+
+        for (FormaPagamento formaPagamento : todasFormasPagamentos) {
+            System.out.println(formaPagamento.getDescricao());
+        }
+    }
+}
