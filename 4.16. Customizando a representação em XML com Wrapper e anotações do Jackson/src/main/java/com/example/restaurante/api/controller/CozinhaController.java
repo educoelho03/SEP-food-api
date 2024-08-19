ee2555cafdbbaf405e0dfbf30f29c280cff4e8e6
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-19 08:51:46 -0300

    manipulando resposta http usando o response Entity

---
+++ 4.16. Customizando a representação em XML com Wrapper e anotações do Jackson/src/main/java/com/example/restaurante/api/controller/CozinhaController.java
@@ -0,0 +1,34 @@
+package com.example.restaurante.api.controller;
+
+import com.example.restaurante.api.model.CozinhasXmlWrapper;
+import com.example.restaurante.domain.model.Cozinha;
+import com.example.restaurante.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id){
+        return cozinhaRepository.buscar(id);
+    }
+}
