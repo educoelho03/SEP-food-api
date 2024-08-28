e9d2d722830d98389da730f4c8288969dce0b420
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-08-28 11:33:09 -0300

    alterando para Spring DATA JPA

---
+++ 5.3. Criando um repositório com Spring Data JPA (SDJ)/src/main/java/com/example/restaurante/domain/service/CadastroEstadoService.java
@@ -0,0 +1,35 @@
+package com.example.restaurante.domain.service;
+
+import com.example.restaurante.domain.exception.EntidadeEmUsoException;
+import com.example.restaurante.domain.exception.EntidadeNaoEncontradaException;
+import com.example.restaurante.domain.model.Estado;
+import com.example.restaurante.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+
+    public Estado salvar(Estado estado){
+        return estadoRepository.salvar(estado);
+    }
+
+    public void excluir(Long id){
+        try{
+            estadoRepository.remover(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de estado com código %d", id));
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format("Não existe um cadastro de estado com código %d", id));
+        }
+    }
+}
