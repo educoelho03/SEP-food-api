120b490826ebd884983cf587bc9eacee9e98a48b
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-09-11 11:07:12 -0300

    usando clausula WHERE no CRITERIA API

---
+++ 5.15. Tornando a consulta com Criteria API com filtros dinâmicos/src/main/java/com/example/restaurante/domain/model/Estado.java
@@ -0,0 +1,20 @@
+package com.example.restaurante.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode
+public class Estado {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
---
+++ 5.15. Tornando a consulta com Criteria API com filtros dinâmicos/src/main/java/com/example/restaurante/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.example.restaurante.infrastructure.repository;
+
+import com.example.restaurante.domain.model.Restaurante;
+import com.example.restaurante.domain.repository.RestauranteRepositoryQueries;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Restaurante> find(String nome,
+                                  BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Restaurante> criteriaQuery = builder.createQuery(Restaurante.class); // criteria é um construtor de clausulas
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class);
+        // root = Restaurante
+
+        var predicates = new ArrayList<Predicate>();
+
+        if(StringUtils.hasText(nome)){
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+        if(taxaFreteInicial != null){
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+        if(taxaFreteFinal != null){
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+
+        criteriaQuery.where(predicates.toArray(new Predicate[0])); // Converter uma ArrayList em um array
+
+        TypedQuery<Restaurante> query = manager.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
+
+}
